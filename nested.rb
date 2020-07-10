106565ac223a4dd199a76b9e665706125766c60a
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-10 00:26:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def dennis_ritchies_language
     :dennis_ritchie => {
     :known_for => "Unix",
     :languages => ["C"]
+    }
   }
-}
-return programmer_hash[:dennis_ritchie][:languages]
+  return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
