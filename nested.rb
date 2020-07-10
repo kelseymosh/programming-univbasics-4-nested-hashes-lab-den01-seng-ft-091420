e635fccd1be3d5fa45df37bbeca1da9bf952630d
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-10 00:21:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -29,7 +29,13 @@ def hopper
 end
 
 def alan_kay_is_known_for
-  programmer_hash[:alan_kay][:known_for]
+  programmer_hash=
+  {
+    :alan_kay => {
+    :known_for => "Object Orientation",
+    :languages => ["Smalltalk", "LISP"]
+  }
+}
 end
 
 def alan_kay_is_known_for
