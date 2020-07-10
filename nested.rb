1ae780c5f714be3d0537d02fee9d4c1527be69d3
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-10 00:20:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,6 +20,10 @@ end
 def hopper
   programmer_hash=
     {
+    :grace_hopper => {
+      :known_for => "COBOL",
+      :languages => ["COBOL", "FORTRAN"]
+    }
   }
   return programmer_hash[:grace_hopper]
 end
