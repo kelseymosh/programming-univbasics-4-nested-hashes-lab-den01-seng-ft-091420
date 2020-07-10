fbd81677f463d2ba4814a3c157a564c952ea5dd9
Kelsey Oshiro <kelseymosh@gmail.com>
2020-07-10 00:36:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -104,52 +104,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
-end
-
-def adding_matz
-  programmer_hash =
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-
-  programmer_hash.merge(:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-      })
-    }
-  } 
-end
-
-def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable.
-  # return the entire updated hash
-
-	programmer_hash =
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
+programmer_hash.merge(:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP","C"]
+}
+)
 end
 
 def adding_to_dennis
